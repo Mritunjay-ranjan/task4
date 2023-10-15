2268bb020ed9b63d01060bd3ac2f8e547b6481ee
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-10-16 00:05:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setUsers(data);
+        }
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Sno</th>
+            <th>Profile Pic</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>E-mail</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <img src={user.profilePic} alt={user.firstName} />
+              </td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.gender}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.domain}</td>
+              <td>{user.ip}</td>
+              <td>{user.university}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
